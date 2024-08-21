9aa6d0322b55e8a6716ccd98f275aa27ac9da16d
jsy5888 <oliviasunjin0318@gmail.com>
2024-08-21 00:32:36 -0400

    CONNECTION_URI

--- server.js
+++ server.js
@@ -631,3 +631,9 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
+
+mongoose.connect( process.env.CONNECTION_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', 
+  { useNewUrlParser: true, useUnifiedTopology: true });
