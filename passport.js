d1a1c69a791f75d07cf601e0ed6e8e8eacf2a9cc
jsy5888 <oliviasunjin0318@gmail.com>
2024-09-26 18:24:21 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,11 +1,12 @@
-const passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
-Models = require('./models.js'),
-passportJWT = require('passport-jwt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Models = require('./models.js');
+const passportJWT = require('passport-jwt');
+const bcrypt = require('bcrypt');
 
-Users = Models.User,
-JWTStrategy = passportJWT.Strategy,
-ExtractJWT = passportJWT.ExtractJwt;
+const Users = Models.User;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
   new LocalStrategy(
@@ -14,40 +15,35 @@ passport.use(
       passwordField: 'Password',
     },
     async (username, password, callback) => {
-      console.log(`${username} ${password}`);
-      await Users.findOne({ Username: username })
-      .then((user) => {
+      try {
+        const user = await Users.findOne({ Username: username });
         if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
+          return callback(null, false, { message: 'Incorrect username or password.' });
         }
-        console.log('finished');
+
+        const isMatch = await bcrypt.compare(password, user.Password);
+        if (!isMatch) {
+          return callback(null, false, { message: 'Incorrect username or password.' });
+        }
+
         return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-      })
+      } catch (error) {
+        return callback(error);
+      }
     }
   )
 );
 
-
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: process.env.JWT_SECRET || 'default_secret'
 }, async (jwtPayload, callback) => {
-  return await Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
+  try {
+    const user = await Users.findById(jwtPayload._id);
+    return callback(null, user);
+  } catch (error) {
+    return callback(error);
+  }
 }));
 
 // // add user
