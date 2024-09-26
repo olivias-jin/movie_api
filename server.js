497131464d28060ec33d1a567b5f64899d3f4fd1
jsy5888 <oliviasunjin0318@gmail.com>
2024-09-26 18:49:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const uuid = require('uuid');
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 const Movies = Models.Movie;
 const Users = Models.User;
